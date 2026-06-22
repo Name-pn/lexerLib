b1d5575917a43184ce8935968b83fe7724881222
Lau-1 <lau-1038254679@yandex.ru>
2026-06-22 21:46:31 +0300

    Add escape sequence support for special characters

--- examples/demo.py
+++ examples/demo.py
@@ -3,7 +3,7 @@ from lexer_lib import RegexNFA
 regex_controller = RegexNFA('examples/test_lang_grammar.txt')
 print('analize')
 regex_controller.automation.pretty_print_grouped()
-regex_controller.setAnalize("123aaaabb")
+regex_controller.setAnalize("123aaaabb()")
 token = regex_controller.next()
 while token:
     print("start: ", token.start_index, " end: ", token.end_index, " id: ", token.pattern_id)
