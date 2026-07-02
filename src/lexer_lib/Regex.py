87981f7f1d528454d3d66c4098f87fe1ac4776ef
Lau-1 <lau-1038254679@yandex.ru>
2026-07-02 18:53:31 +0300

    Add regex definition support

--- src/lexer_lib/Regex.py
+++ src/lexer_lib/Regex.py
@@ -6,13 +6,23 @@ from parser_lib import Grammar
 from .Automation import Automation
 from .Match import Match
 
+def delete_comments(string):
+    return '\n'.join([line for line in string.split("\n") if len(line) > 0 and line[0] != '#'])
+
+
 class Regex():
     def __init__(self, file_name):
         with open(file_name, "r") as f:
             txt = f.read()
         self.txt = txt
-        self.ids = [int(line.split(":")[0]) for line in txt.split("\n")]
-        self.regex_patterns = [(line.split(":")[-1]) for line in txt.split("\n")]
+        self.txt = delete_comments(self.txt)
+        self.ids = [int(line.split(":")[0]) for line in self.txt.split("\n") if ':' in line]
+        self.regex_patterns = [(line.split(":")[-1]) for line in self.txt.split("\n") if ':' in line]
+        self.regex_vars = [line.split("=")[0] for line in self.txt.split("\n") if '=' in line]
+        self.regex_defs = [line.split("=")[-1] for line in self.txt.split("\n") if '=' in line]
+        self.vars_to_def = {}
+        for var, r_def in zip(self.regex_vars, self.regex_defs):
+            self.vars_to_def[var] = "(" + r_def + ")"
         self.grammar = Grammar.load("src/lexer_lib/regex_grammar.txt")
         self.lexer = RegexLexer(self.grammar.enum)
         self.parser = LALRAnalyzerAST(self.grammar)
@@ -41,6 +51,10 @@ class RegexNFA(Regex):
         nfa_list = []
         for id, regex in zip(self.ids, self.regex_patterns):
             tokens = self.lexer.tokenize(regex)
+            for index, token in enumerate(tokens):
+                if token.ttype == "NAME":
+                    tokens = tokens[:index] + self.lexer.tokenize(self.vars_to_def[token.lexem]) + tokens[index + 1:]
+
             print(tokens)
             tree = self.parser.parse(tokens)
             local_nfa = nfa_builder.build(tree, id)
--- src/lexer_lib/RegexLexer.py
+++ src/lexer_lib/RegexLexer.py
@@ -13,6 +13,8 @@ class RegexLexer():
         self.types = types
 
     def get_token(self, c):
+        if len(c) > 1:
+            return LTerminal(c, self.types['NAME'].name)
         match (c):
             case '*':
                 return Terminal(self.types['ASTERISK'].name)
@@ -22,6 +24,10 @@ class RegexLexer():
                 return Terminal(self.types['LEFT_BRACKET'].name)
             case ')':
                 return Terminal(self.types['RIGHT_BRACKET'].name)
+            case '{':
+                return Terminal(self.types['LEFT_CURLY_BRACKET'].name)
+            case '}':
+                return Terminal(self.types['RIGHT_CURLY_BRACKET'].name)
             case c if 0 <= ord(c) <= 255:
                 return LTerminal(c, self.types['SYMBOL'].name)
             case _:
@@ -30,22 +36,41 @@ class RegexLexer():
     def get_escape(self, c):
         return LTerminal(c, self.types['SYMBOL'].name)
 
+    def get_name_string(self, string: str, index: int):
+        res = ""
+        if len(string) > index and (string[index] >= 'a' and string[index] <= 'z' or\
+            string[index] >= 'A' and string[index] <= 'Z' or string[index] == '_'):
+            res = res + (string[index])
+            index += 1
+        while len(string) > index and (string[index] >= 'a' and string[index] <= 'z' or\
+            string[index] >= 'A' and string[index] <= 'Z' or\
+            string[index] >= '0' and string[index] <= '9' or string[index] == '_'):
+            res = res + (string[index])
+            index += 1
+        if not res:
+            raise SyntaxError("Empty regex name calling")
+        if len(string) <= index or string[index] != '}':
+            raise SyntaxError("There isnt closing curly bracket")
+        return res, index
+
     def tokenize(self, string):
         res = []
         index = 0
         while index < len(string):
-            is_escape = False
-            if string[index] != "\\":
-                char = string[index]
-            else:
-                is_escape = True
+            if string[index] == "\\":
                 index += 1
                 if index >= len(string):
                     raise SyntaxError("After \\ must be symbol")
-                char = self.ESCAPE_MAP.get(string[index], string[index]) 
-            if is_escape:
+                char = self.ESCAPE_MAP.get(string[index], string[index])
                 res.append(self.get_escape(char))
+            elif string[index] == "{":
+                index += 1
+                name, index = self.get_name_string(string, index)
+                #res.append(self.get_token('{'))
+                res.append(self.get_token(name))
+                #res.append(self.get_token('}'))
             else:
+                char = string[index]
                 res.append(self.get_token(char))
             index += 1
         return res
